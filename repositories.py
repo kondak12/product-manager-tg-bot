bcad56e6a5ca6b9d7c62f9a1f73af0a27341a8b1
kondak12 <ivan.kondakov.98@bk.ru>
2025-09-19 01:01:04 +0300

    feature(repositories) add base repo for Tasks table

--- repositories.py
+++ repositories.py
@@ -1,5 +1,7 @@
 import aiosqlite
 
+from models import Task
+
 
 class UserRepository:
 
@@ -10,8 +12,7 @@ class UserRepository:
         sql_command = """
         CREATE TABLE IF NOT EXISTS `Users` (
             `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-            `username` STRING NOT NULL,
-            `pincode` INTEGER NOT NULL
+            `pincode` INTEGER
         );
     """
         async with aiosqlite.connect(self.__db_path) as db:
@@ -39,3 +40,79 @@ class UserRepository:
         pincode = await cursor.fetchone()
 
         return pincode
+
+
+class TaskRepository:
+    def __init__(self, db_path: str):
+        self.__db_path = db_path
+
+    async def init_tables(self) -> None:
+        sql_command = """
+                CREATE TABLE IF NOT EXISTS `Tasks` (
+                    `user_id` TEXT NOT NULL,
+                    `id` INTEGER AUTOINCREMENT,
+                    `name` INTEGER NOT NULL,
+                    `description` TEXT,
+                    `deadline` DATETIME NOT NULL,
+                    `created_at` TEXT DEFAULT CURRENT_TIMESTAMP
+                    FOREIGN KEY (`user_id`) REFERENCES `Users`.`id`
+                );
+            """
+        async with aiosqlite.connect(self.__db_path) as db:
+            await db.execute(sql_command)
+            await db.commit()
+
+    async def add_new_task(self,
+                          user_id: int,
+                          name: str,
+                          description: str,
+                          deadline: str
+                          ) -> Task:
+
+        sql_command = """
+                INSERT INTO `Tasks` (`name`, `description`, `deadline`)
+                    WHERE `user_id` == ?
+                    VALUES (?, ?, ?);
+            """
+
+        async with aiosqlite.connect(self.__db_path) as db:
+            db.row_factory = aiosqlite.Row
+
+            await db.execute(sql_command, [user_id, name, description, deadline])
+            await db.commit()
+
+            cursor = await db.execute(
+                """
+                SELECT * FROM `Tasks` 
+                    WHERE `user_id` = ? AND `name` == ?;
+                """, [user_id, name]
+            )
+            row_task = await cursor.fetchone()
+            return Task(**dict(row_task))
+
+    async def get_task(self, user_id: int, task_name: str) -> Task:
+        async with aiosqlite.connect(self.__db_path) as db:
+            cursor = await db.execute(
+                """
+                SELECT * FROM `Tasks` 
+                    WHERE `user_id` = ? AND `name` == ?;
+                """, [user_id, task_name]
+            )
+            row_task = await cursor.fetchone()
+            return Task(**dict(row_task))
+
+    async def get_task_list(self, user_id: str) -> list[Task]:
+        async with aiosqlite.connect(self.__db_path) as db:
+            db.row_factory = aiosqlite.Row
+
+            cursor = await db.execute(
+                """
+                SELECT * FROM `Tasks`"
+                    WHERE `user_id` == ?;
+                """, [user_id]
+            )
+
+            return [
+                Task(**dict(row_task))
+                for row_task in await cursor.fetchall()
+            ]
